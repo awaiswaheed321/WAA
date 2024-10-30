994ca2d7e46dfa85d129bf7a8e9041bb97d3d19a
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-30 15:16:34 -0500

    Create User.java

---
+++ src/main/java/edu/miu/labs/entities/User.java
@@ -0,0 +1,34 @@
+package edu.miu.labs.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    List<Post> posts;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+}
